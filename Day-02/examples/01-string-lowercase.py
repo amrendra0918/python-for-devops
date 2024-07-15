9afa249e592c9cd9c1f5cef823eefd02d7111d77
Amrendra Pratap Singh <130237717+amrendra0918@users.noreply.github.com>
2024-07-15 17:09:17 +0000

    lowercase update

--- Day-02/examples/01-string-lowercase.py
+++ Day-02/examples/01-string-lowercase.py
@@ -3,3 +3,7 @@ uppercase = text.upper()
 lowercase = text.lower()
 print("Uppercase:", uppercase)
 print("Lowercase:", lowercase)
+
+text2 = "This is awesome"
+print("uppercase:", text2.upper())
+print("lowercase:", text2.lower())
