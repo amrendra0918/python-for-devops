ce1faf401ca57f2826eb323d2759db9861a004a9
Amrendra Pratap Singh <130237717+amrendra0918@users.noreply.github.com>
2024-07-15 17:14:50 +0000

    update

--- Day-02/examples/01-string-concat.py
+++ Day-02/examples/01-string-concat.py
@@ -2,3 +2,9 @@ str1 = "Hello"
 str2 = "World"
 result = str1 + " " + str2
 print(result)
+
+first = "Amrendra"
+middle =  "Pratap"
+last = "Singh"
+
+print("Entire Name:", first + " " + middle + " " + last)
--- Day-02/examples/01-string-len.py
+++ Day-02/examples/01-string-len.py
@@ -1,3 +1,6 @@
 text = "Python is awesome"
 length = len(text)
 print("Length of the string:", length)
+
+text2 = "This is awesome"
+print("length of the sentence:", len(text2))
