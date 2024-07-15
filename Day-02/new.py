846d7f4b6181559d25325d47ddf5df384d98f076
Amrendra Pratap Singh <130237717+amrendra0918@users.noreply.github.com>
2024-07-15 17:04:05 +0000

    new.py

---
+++ Day-02/new.py
@@ -0,0 +1,15 @@
+arn = "arn:aws:iam::123456789:user/jagjit"
+wudu = "arn:aws:iam::12345678987:user/singh"
+print(arn.split("/")[1]);
+print(wudu.split(":")[4]);
+print(wudu.upper())
+
+qwe = "ek do teen"
+rty = "chaar paanch"
+print(qwe + " " + rty)
+
+text = "Amrendra pratap singh"
+print("length of the name is:", len(text))
+
+text2 = " amar    "
+print(text2.strip())
