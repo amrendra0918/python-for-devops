3498c287e6eeeadecbe5bedf32783c13c5b2423c
Amrendra Pratap Singh <130237717+amrendra0918@users.noreply.github.com>
2024-07-16 14:09:09 +0000

    dayfinale

--- Day-02/examples/03-regex-search.py
+++ Day-02/examples/03-regex-search.py
@@ -8,3 +8,14 @@ if search:
     print("Pattern found:", search.group())
 else:
     print("Pattern not found")
+
+import re
+text2 = "Nature is beautiful"
+x = r"beautiful"
+search2 = re.search(x, text2)
+if search2:
+    print("Pattern found:", search2.group())
+else:
+    print("Patter not found")
+
+
